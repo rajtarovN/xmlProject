bfda5fcf2d0f0c06e7b14c8c581417eb0d69e2f4
oljamirilovic <69585255+oljamirilovic@users.noreply.github.com>
2022-02-06 16:30:32 +0100

    Sluzbenik back created

---
+++ SluzbenikAplikacija/sluzbenik_back/src/main/java/com/example/sluzbenik_back/model/liste/JaxbLista.java
@@ -0,0 +1,30 @@
+package com.example.sluzbenik_back.model.liste;
+
+
+import com.example.sluzbenik_back.model.korisnik.Korisnik;
+
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+import java.util.ArrayList;
+import java.util.List;
+
+//When doing so you have to specify which classes could possibly be in the list using @XmlSeeAlso
+@XmlRootElement
+@XmlSeeAlso({ Korisnik.class})
+public class JaxbLista<T> {
+  private List<T> lista = new ArrayList<>();
+
+
+  public JaxbLista() {
+  }
+
+  public JaxbLista(List<T> lista) {
+    this.lista = lista;
+  }
+
+  @XmlAnyElement
+  public List<T> getLista() {
+    return this.lista;
+  }
+}
