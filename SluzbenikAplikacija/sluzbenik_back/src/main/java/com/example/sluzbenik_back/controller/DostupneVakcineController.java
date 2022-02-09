a580da284217198aa3d7907b30a3e362083a564a
Teodora Rajnovic <rajnovicteodora@gmail.com>
2022-02-09 13:23:02 +0100

    Add inventory

---
+++ SluzbenikAplikacija/sluzbenik_back/src/main/java/com/example/sluzbenik_back/controller/DostupneVakcineController.java
@@ -0,0 +1,54 @@
+package com.example.sluzbenik_back.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.xmldb.api.modules.XMLResource;
+
+import com.example.sluzbenik_back.service.DostupneVakcineService;
+
+@Controller
+@RequestMapping(value = "/api/zalihe")
+public class DostupneVakcineController {
+
+	private DostupneVakcineService dostupneVakcineService;
+
+	private static final Logger LOG = LoggerFactory.getLogger(DostupneVakcineController.class);
+
+	@Autowired
+	public DostupneVakcineController(DostupneVakcineService dostupneVakcineService) {
+		this.dostupneVakcineService = dostupneVakcineService;
+	}
+
+	@GetMapping(produces = "application/xml")
+	public ResponseEntity<String> getXML() {
+		LOG.info("cao");
+		try {
+			XMLResource zalihe = dostupneVakcineService.readXML("zalihe");
+			return new ResponseEntity<>(zalihe.getContent().toString(), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+
+	}
+
+	@PostMapping(consumes = "application/xml")
+	public ResponseEntity<?> saveXML(@RequestBody String content) {
+		String documentId = "zalihe";
+
+		try {
+			dostupneVakcineService.saveXML(documentId, content);
+			return new ResponseEntity<>(HttpStatus.CREATED);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+}
--- SluzbenikAplikacija/sluzbenik_back/src/main/java/com/example/sluzbenik_back/service/DostupneVakcineService.java
+++ SluzbenikAplikacija/sluzbenik_back/src/main/java/com/example/sluzbenik_back/service/DostupneVakcineService.java
@@ -1,4 +1,4 @@
-package com.example.demo.service;
+package com.example.sluzbenik_back.service;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
@@ -12,34 +12,34 @@ import org.apache.commons.io.input.ReaderInputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.model.interesovanje.Interesovanje;
-import com.example.demo.repository.InteresovanjeRepository;
+import com.example.sluzbenik_back.model.dostupne_vakcine.Zalihe;
+import com.example.sluzbenik_back.repository.DostupneVakcineRepository;
 
 @Service
-public class InteresovanjeService extends AbstractService{
-	
-	@Autowired
-	public InteresovanjeService(InteresovanjeRepository interesovanjeRepository) {
+public class DostupneVakcineService extends AbstractService {
 
-		super(interesovanjeRepository, "/db/portal/interesovanje", "/interesovanje/" );
+	@Autowired
+	public DostupneVakcineService(DostupneVakcineRepository dostupneVakcineRepository) {
+
+		super(dostupneVakcineRepository, "/db/sluzbenik/zalihe", "/zalihe/");
 	}
-	
+
 	@Override
 	public void saveXML(String documentId, String content) throws Exception {
 
 		InputStream inputStream = new ReaderInputStream(new StringReader(content));
 
-		JAXBContext context = JAXBContext.newInstance(Interesovanje.class);
+		JAXBContext context = JAXBContext.newInstance(Zalihe.class);
 		Unmarshaller unmarshaller = context.createUnmarshaller();
 
-		Interesovanje interesovanje = (Interesovanje) unmarshaller.unmarshal(inputStream);
+		Zalihe zalihe = (Zalihe) unmarshaller.unmarshal(inputStream);
 
 		Marshaller marshaller = context.createMarshaller();
 		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 
 		ByteArrayOutputStream stream = new ByteArrayOutputStream();
 
-		marshaller.marshal(interesovanje, stream);
+		marshaller.marshal(zalihe, stream);
 
 		String finalString = new String(stream.toByteArray());
 		System.out.println(finalString);
