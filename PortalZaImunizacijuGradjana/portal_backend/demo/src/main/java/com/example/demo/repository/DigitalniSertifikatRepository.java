5937d6a99c627ef67e038a60215481acebaf4c63
oljamirilovic <69585255+oljamirilovic@users.noreply.github.com>
2022-02-16 19:13:14 +0100

    PrikazListeSertifikata finished

--- PortalZaImunizacijuGradjana/portal_backend/demo/src/main/java/com/example/demo/repository/DigitalniSertifikatRepository.java
+++ PortalZaImunizacijuGradjana/portal_backend/demo/src/main/java/com/example/demo/repository/DigitalniSertifikatRepository.java
@@ -1,7 +1,41 @@
 package com.example.demo.repository;
 
+import com.example.demo.util.DBManager;
+import com.example.demo.util.FusekiManager;
+import org.exist.xupdate.XUpdateProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.xmldb.api.base.XMLDBException;
+import org.xmldb.api.modules.XMLResource;
+
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class DigitalniSertifikatRepository extends RepositoryInterface {
+
+    private String collectionId = "/db/portal/lista_sertifikata";
+
+    public static final String SPARQL_FILE = "src/main/resources/static/sparql/sertifikat/";
+
+    @Autowired
+    private FusekiManager fusekiManager;
+
+    @Autowired
+    private DBManager dbManager;
+
+    public XMLResource pronadjiPoId(String id) throws IllegalAccessException, JAXBException, InstantiationException, IOException, XMLDBException, ClassNotFoundException {
+        return dbManager.readFileFromDB("sertifikat_"+id+".xml", collectionId);
+    }
+
+    public List<String> pronadjiPoJmbg(String jmbg ) throws Exception {
+        List<String> ids = new ArrayList<>();
+        ArrayList<String> params = new ArrayList<>();
+        params.add("\"" +jmbg+"\"");
+
+        ids = this.fusekiManager.query("/lista_sertifikata", SPARQL_FILE + "sertifikat_jmbg.rq", params);
+        return ids;
+    }
 }
